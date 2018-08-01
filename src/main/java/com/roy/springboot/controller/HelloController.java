8f343c358d927dab5f0b59073ae6ba86469fe2bf
test <ranjun_liu@126.com>
2018-08-01 18:12:00 +0800

    thymeleaf

--- src/main/java/com/roy/springboot/controller/HelloController.java
+++ src/main/java/com/roy/springboot/controller/HelloController.java
@@ -1,17 +1,34 @@
 package com.roy.springboot.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Map;
+
+@Controller
 public class HelloController {
 
     @Value("${person.name}")
     private String name;
 
+    @ResponseBody
     @RequestMapping("/hello")
     public String hello(){
         return "Hello " + name;
+    }
+
+    @RequestMapping("/success")
+    public String success(Map<String,Object> map){
+
+        map.put("hello","<h1>你好</h1>");
+        map.put("users", Arrays.asList("roy1","adf","123213"));
+
+        //classpath:templates/success.html
+        return "success";
     }
 }
--- src/test/java/com/roy/springboot/SpringbootApplicationTests.java
+++ src/test/java/com/roy/springboot/SpringbootApplicationTests.java
@@ -3,6 +3,8 @@ package com.roy.springboot;
 import com.roy.springboot.bean.Person;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
@@ -18,9 +20,24 @@ public class SpringbootApplicationTests {
 	@Autowired
 	ApplicationContext context;
 
+	//记录器
+	Logger logger = LoggerFactory.getLogger(getClass());
+
 	@Test
 	public void contextLoads() {
-		System.out.println(person);
+
+		//日志的级别，由低到高，可以调整输出的日志级别：不需要注释，高级别的日志信息可以显示，低级别的就不在显示
+
+		logger.trace("跟踪轨迹");
+
+		logger.debug("调试信息");
+
+		//SpringBoot默认的级别是Info级别
+		logger.info("Info信息");
+
+		logger.warn("警告信息");
+
+		logger.error("错误信息");
 	}
 
 	@Test
